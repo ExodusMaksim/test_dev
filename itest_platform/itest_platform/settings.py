612bec7e94566c3a74a70ec35cb8f531ab476827
ExodusMaksim <1029152854@qq.com>
2021-07-20 22:45:46 +0800

    模块路径重构

--- itest_platform/itest_platform/settings.py
+++ itest_platform/itest_platform/settings.py
@@ -80,6 +80,14 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': 'my.cnf',
+#         }
+#     }
+# }
 
 
 # Password validation
@@ -119,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- itest_platform/project_manage/admin.py
+++ itest_platform/project_manage/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Project, Module
 # Register your models here.
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["name", "describe", "status", "create_time"]
+
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ["name", "describe", "project", "create_time"]
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Module, ModuleAdmin)
